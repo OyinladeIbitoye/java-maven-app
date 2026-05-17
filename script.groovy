17fd4019503f2e3f903060534d084bb05cb25cac
OyinladeIbitoye <oyinladeibitoye@gmail.com>
2026-05-17 18:10:37 +0100

    corrected scriptt

--- script.groovy
+++ script.groovy
@@ -1,23 +1,19 @@
-def buildApp() {
+def buildJar() {
     echo 'building the application...'
     sh 'mvn package'
 }
 
-// def buildImage() {
-//     echo "building the docker image..."
-//     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-//         sh 'docker build -t esthertwn/demo-app:jma-2.0 .'
-//         sh 'echo $PASS | docker login -u $USER --password-stdin'
-//         sh 'docker push esthertwn/demo-app:jma-2.0'
-//     }
-// }
-def testApp() {
-    echo 'testing the application......'
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t oluwasparkle/demo-app:jma-2.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push oluwasparkle/demo-app:jma-2.0'
+    }
 }
 
 def deployApp() {
     echo 'deploying the application...'
-    echo "deploying version ${params.VERSION}"
 }
 
 return this
